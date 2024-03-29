784216647471d40867464031dc14d8db0e67bdf7
Dhiya Ulhaq <dhiya@gmail.com>
2024-03-29 15:24:38 +0700

    migration, database seeder, factory

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,18 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\clear;
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
+        $todos = Todo::where('user_id', auth()->user()->id)->get();
+        dd($todos);
         return view('todo.index');
     }
+
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
         return view('todo.create');
     }
+
     public function edit()
     {
         return view('todo.edit');
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'is_admin' => false
         ];
     }
 
@@ -38,7 +39,6 @@ class UserFactory extends Factory
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
+            'email_verified_at' => null,]);
+        }
 }
